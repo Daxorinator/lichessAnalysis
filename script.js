f76bd5ab239205c36389596c5ab13e39d7c5bd65
Seán Kelly <sean@override.online>
2023-11-18 12:35:25 +0000

    Enable bot game support; Add convertPGNToMoves(); Rewfactor from /api/import to /analysis/png to fix CORS issue; Let and var to const

--- script.js
+++ script.js
@@ -2,9 +2,20 @@
 checkGameStatus()
 
 function checkGameStatus() {
-    document.arrive(".game-review-buttons-review", function() {
-        // Find chess.com analysisButton
-        var analysisButton = document.querySelector(".ui_v5-button-component.ui_v5-button-primary.ui_v5-button-full.game-review-buttons-button")
+    // For bots we check for the quick analysis button
+    document.arrive(".quick-analysis-buttons", () => {
+        const analysisButton = document.querySelector(".ui_v5-button-component.ui_v5-button-basic.ui_v5-button-full.quick-analysis-button");
+        const isButtonValid = analysisButton?.className === "ui_v5-button-component ui_v5-button-basic ui_v5-button-full quick-analysis-button";
+        if (isButtonValid) {
+          Arrive.unbindAllArrive();
+          injectButton(analysisButton);
+          checkGameStatus();
+        }
+      });
+
+    // For normal games we check for the regular analysis button
+    document.arrive(".game-review-buttons-review", () => {
+        const analysisButton = document.querySelector(".ui_v5-button-component.ui_v5-button-primary.ui_v5-button-full.game-review-buttons-button")
         if (analysisButton.className == "ui_v5-button-component ui_v5-button-primary ui_v5-button-full game-review-buttons-button"){
             Arrive.unbindAllArrive();
             injectButton(analysisButton);
@@ -16,7 +27,7 @@ function checkGameStatus() {
 // Injects a button similar to chess.com's native "Analysis" button 
 function injectButton(analysisButton){
     // Duplicate the original button
-    let newButton = analysisButton.cloneNode("deep");
+    const newButton = analysisButton.cloneNode("deep");
     // Style it and link it to the Lichess import function.
     newButton.childNodes[2].innerText = "Lichess Analysis";
     newButton.style.margin = "8px 0px 0px 0px";
@@ -29,8 +40,22 @@ function injectButton(analysisButton){
         sendToLichess();
     });
     // Append back into the DOM
-    let parentNode = analysisButton.parentNode;
+    const parentNode = analysisButton.parentNode;
     parentNode.append(newButton);
+}
+
+function convertPGNToMoves(pgn) {
+    const lines = pgn.split("\n").map((s) => s.trim());
+
+    // Finding index of the empty newline, after that we have our PGN.
+    const idx = lines.map(Boolean).indexOf(false);
+    const moves = lines.slice(idx + 1).join();
+
+    if (!moves || moves.length === 0) {
+      console.error("Invalid PGN input. No moves found.");
+      return "";
+    }
+    return moves
 }
 
 // Make request to Lichess through the API (fetch)
@@ -38,43 +63,31 @@ function sendToLichess(){
     // 1. Get PGN
 
     // Get and click download button on chess.com
-    let downloadButton = document.getElementsByClassName("icon-font-chess share live-game-buttons-button")[0];
+    const downloadButton = document.getElementsByClassName("icon-font-chess share live-game-buttons-button")[0];
     downloadButton.click();
 
     // Wait for share tab to pop up
-    document.arrive(".share-menu-tab-pgn-textarea", function()  {
+    document.arrive(".share-menu-tab-pgn-textarea", () => {
         Arrive.unbindAllArrive();
 
         // Get PGN from text Area
-        var PGN = document.getElementsByClassName("share-menu-tab-pgn-textarea")[0].value;
+        const PGN = document.getElementsByClassName("share-menu-tab-pgn-textarea")[0].value;
 
         // Exit out of download view (x button)
         document.querySelector("div.icon-font-chess.x.ui_outside-close-icon").click();
 
-        // 2. Send a POST request to Lichess to import the current game
-        let importUrl = "https://lichess.org/api/import"
-        let req = {pgn: PGN};
-        post(importUrl, req)
-            .then((response) => {
-                // Open the page on a new tab
-                let url = response["url"] ? response["url"] : "";
-                if (url) {
-                    let lichessPage = window.open(url);
-                } else alert("Could not import game");
-
-            }).catch((e) => {
-            alert("Error getting response from lichess.org");
-            throw new Error("Response error");
-        });
+        // 2. Open the game in a Lichess analysis tab.
+        const importUrl = "https://lichess.org/analysis/pgn"
+        window.open(`${importUrl}/${convertPGNToMoves(PGN)}`);
     });
 }
 
 // async POST function
 async function post(url = '', data = {}) {
-    var formBody = [];
-    for (var property in data) {
-        var encodedKey = encodeURIComponent(property);
-        var encodedValue = encodeURIComponent(data[property]);
+    const formBody = [];
+    for (const property in data) {
+        const encodedKey = encodeURIComponent(property);
+        const encodedValue = encodeURIComponent(data[property]);
         formBody.push(encodedKey + "=" + encodedValue);
     }
     const response = await fetch(url, {
